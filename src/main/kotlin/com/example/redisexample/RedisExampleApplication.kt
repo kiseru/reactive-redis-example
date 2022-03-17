df0afb4ff5296229f603d4c08eb957069cbefeea
Alexandr Kiselev <kis.alex97@gmail.com>
2022-03-17 18:52:48 +0300

    rewritten geography application runner with using coroutines

--- src/main/kotlin/com/example/redisexample/RedisExampleApplication.kt
+++ src/main/kotlin/com/example/redisexample/RedisExampleApplication.kt
@@ -1,5 +1,8 @@
 package com.example.redisexample
 
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -10,10 +13,7 @@ import org.springframework.data.geo.Circle
 import org.springframework.data.geo.Distance
 import org.springframework.data.geo.Point
 import org.springframework.data.redis.connection.RedisGeoCommands
-import org.springframework.data.redis.core.ReactiveRedisTemplate
-import org.springframework.data.redis.core.leftPopAndAwait
-import org.springframework.data.redis.core.leftPushAllAndAwait
-import reactor.core.publisher.Flux
+import org.springframework.data.redis.core.*
 
 @SpringBootApplication
 @Configuration
@@ -21,19 +21,21 @@ class RedisExampleApplication {
 
     @Bean
     fun geography(template: ReactiveRedisTemplate<String, String>) = ApplicationRunner {
-        val sicily = "Sicily"
-        val geoTemplate = template.opsForGeo()
-        val mapOfPoints = mapOf(
-            "Arigento" to Point(13.361389, 38.11555556),
-            "Catania" to Point(15.0876269, 37.502669),
-            "Palermo" to Point(13.5833333, 37.316667),
-        )
-        Flux.fromIterable(mapOfPoints.entries)
-            .flatMap { geoTemplate.add(sicily, it.value, it.key) }
-            .thenMany(geoTemplate.radius(sicily, Circle(Point(13.583333, 37.31667), Distance(10.0, RedisGeoCommands.DistanceUnit.KILOMETERS))))
-            .map { it.content.name }
-            .doOnNext(::println)
-            .blockLast()
+        runBlocking {
+            val sicily = "Sicily"
+            val geoTemplate = template.opsForGeo()
+            val mapOfPoints = mapOf(
+                "Arigento" to Point(13.361389, 38.11555556),
+                "Catania" to Point(15.0876269, 37.502669),
+                "Palermo" to Point(13.5833333, 37.316667),
+            )
+            mapOfPoints.entries.asFlow()
+                .collect { geoTemplate.addAndAwait(sicily, it.value, it.key) }
+            val circle = Circle(Point(13.583333, 37.31667), Distance(10.0, RedisGeoCommands.DistanceUnit.KILOMETERS))
+            geoTemplate.radiusAsFlow(sicily, circle)
+                .map { it.content.name }
+                .collect(::println)
+        }
     }
 
     @Bean
